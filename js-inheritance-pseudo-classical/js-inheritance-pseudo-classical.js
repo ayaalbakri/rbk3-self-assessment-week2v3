c51a66e66d5deaef8a8a04c00fb96396d549608e
ayaalbakri <aya.albakri3@gmail.com>
2017-09-03 09:14:56 -0700

    haha!

--- js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
+++ js-inheritance-pseudo-classical/js-inheritance-pseudo-classical.js
@@ -39,3 +39,31 @@ var makeSmartPhone = function(phoneNumber, email) {
 };
 
 // your code is here
+var makePhone = function  (phoneNumber) {
+  // body... 
+  this.phoneNumber = phoneNumber ; 
+
+}
+
+
+makePhone.prototype.send = function(recipientPhoneNumber, message) {
+    return 'sending the message "' + message + '" to the phone number ' + recipientPhoneNumber + ' from ' + this.phoneNumber;
+  };
+
+  var makeSmartPhone = function (phoneNumber, email) {
+    /* body... */
+    makePhone.call(this , phoneNumber);
+    this.email = email;
+  }
+
+  makeSmartPhone.prototype = Object.create(makePhone.prototype);
+  makeSmartPhone.prototype.constructor = makeSmartPhone;
+  makeSmartPhone.prototype.send  = function(recipientPhoneNumberOrEmail, message) {
+    if (typeof recipientPhoneNumberOrEmail === 'number') {
+      // We need `.call` here to make sure that `this` will reference our smart phone in makePhone's send
+      // return oldSend.call(this, recipientPhoneNumberOrEmail, message); 
+      return this.send(recipientPhoneNumber, message)
+    } else {
+      return 'sending the message "' + message + '" to email ' + recipientPhoneNumberOrEmail + ' from ' + this.email;
+    }
+  };
